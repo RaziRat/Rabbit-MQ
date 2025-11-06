3f5b70febdd83b594ff15c128ef1efffbccf5061
Razi Abidi <alirazi719@gmail.com>
2025-11-06 13:31:17 +0500

    Fanout

--- Producer.js
+++ Producer.js
@@ -2,19 +2,20 @@ const amqp = require("amqplib");
 const { send } = require("process");
 const { buffer } = require("stream/consumers");
 
-const sendMsg = async (routingKey,message ) => {
+const annProduct = async (product) => {
 try{
 
     const connection = await amqp.connect("amqp://localhost");
     const channel = await connection.createChannel();
-    const exchange = "notification_exchange";
-    const exchangeType = "topic";
+    const exchange = "new_product";
+    const exchangeType = "fanout";
 
     await channel.assertExchange(exchange, exchangeType, {durable: true});
-    channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)), {persistent: true});
-    console.log(" [x] Sent '%s':'%s'",routingKey,JSON.stringify(message));
-    console.log(`Message has sent with Routing Key as ${routingKey} and content as ${message}`);
+    const message = JSON.stringify(product)
 
+    channel.publish(exchange, "", Buffer.from(message), {persistent: true});
+    console.log("Sent => ", message);
+    
     setTimeout(() =>{
         connection.close();
     },500);
@@ -23,51 +24,5 @@ catch(error){
     console.log(error);
 }
 }
-sendMsg("order.placed",{ orderId: 12345 , status: "Placed"});
-sendMsg("payment.processed", {paymentId: 67890, status: "processed"});
-
-
-// async function sendMail() {
-//     try{
-//         const connection = await amqp.connect("amqp://localhost");
-//         const channel = await connection.createChannel()
-//         const exchange = "mail_exchange";
-//         const routingKey1 = "send_Mail_to_sub_users";
-//         const routingKey2 = "send_Mail_to_users";
-
-//         const message1 = {
-//             to: "subUsers@gmail.com",
-//             from: "abidisahib719@gmail.com",
-//             subject: "Greets",
-//             body: "Hello Subscribers reedeem your Gift"
-//         }
-//         const message2 = {
-//             to: "Users@gmail.com",
-//             from: "abidisahib719@gmail.com",
-//             subject: "Greets",
-//             body: "Hey Users Subscribe to get Gift"
-//         }
-//         await channel.assertExchange(exchange, "direct", {durable: false});
-//         await channel.assertQueue("sub_users_mail_queue", {durable: false});
-//         await channel.bindQueue("sub_users_mail_queue", exchange, routingKey1)
-
-//         await channel.assertQueue("users_mail_queue", {durable: false});
-//         await channel.bindQueue("users_mail_queue", exchange, routingKey2)
-
-//         channel.publish(exchange, routingKey1, Buffer.from(JSON.stringify(message1)));
-//         console.log("Mail sent to Sub_users", message1);
-
-//         channel.publish(exchange, routingKey2, Buffer.from(JSON.stringify(message2)));
-//         console.log("Mail sent to users", message2);
-
-//         setTimeout(() =>{
-//             connection.close();
-//         },500)
-
-//     }
-//     catch (error){
-//     console.log(error);
-//     }
-    
-// }
-// sendMail();
+annProduct({ orderId: 12345 , name: "iphone 18", price: 400000});
+ 
